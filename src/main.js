5398fa606d4129b7b3206787e71dc9554b97246b
tztomer <mytztomer@gmail.com>
2022-08-04 01:04:42 +0300

    dsfd

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
 import { createApp } from 'vue';
-import './assets/styles/main.scss';
+import '/src/styles/style.scss';
 import App from './App.vue';
-const app = createApp(App);
-// app.directive('font-size', {
-//   beforeMount: (el, binding) => {
-//     el.style.fontSize = 20 + 'px';
-//   },
-//   updated: (el, binding) => {
-//     el.style.fontSize = 20 + 'px';
-//   },
-// });
-app.mount('#app');
+
+createApp(App).mount('#app');
