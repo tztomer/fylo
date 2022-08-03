2cfb6401caab430cc4398303daed94d105a5df02
tztomer <mytztomer@gmail.com>
2022-08-04 01:06:31 +0300

    sdfsdfsdfsd

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         </div>
 
         <h1>All your files in one secure location, accessible anywhere.</h1>
-        <p
+        <p v-font-size
           >Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and
           co-workers.
         </p>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue';
-import '/src/styles/style.scss';
+import './assets/styles/main.scss';
 import App from './App.vue';
-
-createApp(App).mount('#app');
+const app = createApp(App);
+app.directive('font-size', {
+  beforeMount: (el, binding) => {
+    el.style.fontSize = 20 + 'px';
+  },
+  updated: (el, binding) => {
+    el.style.fontSize = 20 + 'px';
+  },
+});
+app.mount('#app');
