260c4dffcd433c4282ac89c027b5360fabb1771b
tztomer <mytztomer@gmail.com>
2022-08-04 16:40:04 +0300

    fdsf

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue';
 import './assets/styles/main.scss';
 import App from './App.vue';
 const app = createApp(App);
-app.directive('font-size', {
-  beforeMount: (el, binding) => {
-    el.style.fontSize = 20 + 'px';
-  },
-  updated: (el, binding) => {
-    el.style.fontSize = 20 + 'px';
-  },
-});
+// app.directive('font-size', {
+//   beforeMount: (el, binding) => {
+//     el.style.fontSize = 20 + 'px';
+//   },
+//   updated: (el, binding) => {
+//     el.style.fontSize = 20 + 'px';
+//   },
+// });
 app.mount('#app');
